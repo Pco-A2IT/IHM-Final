b51518252059ce933586cb2f10feb193b6b9a180
luigi-bardi <luigi.bardi@insa-lyon.fr>
2016-12-15 23:59:13 +0100

    Update Prise_RDV.js

--- Prise_RDV.js
+++ Prise_RDV.js
@@ -3,5 +3,6 @@ var boutonElt = document.getElementById("RetourDossierPatient");
 boutonElt.addEventListener("click", function () {
     console.log("clic");
     console.log("Le récapitulatif a été envoyé");
-    window.open('Dossier_Patient.html','nom_de_ma_popup','fullscreen=yes, menubar=no, scrollbars=no'); 
+    top.location.href='Dossier_Patient.html';
+
 });
