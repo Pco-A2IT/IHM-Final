ef40b8de9c64b04f93e8572b27f2b0081f083459
Zizi30 <zineb.sen30@gmail.com>
2016-12-23 11:36:50 +0100

    Delete Prise_RDV.js

--- Prise_RDV.js
+++
@@ -1,8 +0,0 @@
-var boutonElt = document.getElementById("RetourDossierPatient");
-
-boutonElt.addEventListener("click", function () {
-    console.log("clic");
-    console.log("Le récapitulatif a été envoyé");
-    top.location.href='Dossier_Patient.html';
-
-});
