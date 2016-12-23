19861620d320d203d0cfb492391514983af8804d
Zizi30 <zineb.sen30@gmail.com>
2016-12-23 11:30:53 +0100

    Update Dossier_Patient.js

--- Dossier_Patient.js
+++ Dossier_Patient.js
@@ -1,38 +1,10 @@
-var boutonElt = document.getElementById("menu1");
-var boutonElt2 = document.getElementById("menu5");
-var boutonElt3 = document.getElementById("menu3");
-var boutonElt4 = document.getElementById("menu2");
-var boutonElt5 = document.getElementById("menu4");
-var boutonElt6 = document.getElementById("myButton1");
-var boutonElt7 = document.getElementById("myButton2");
+
+var boutonElt6 = document.getElementById("btn_prise_rdv");
+var boutonElt7 = document.getElementById("btn_recap");
 var boutonElt8 = document.getElementById("RetourDossierPatient");
 // Ajout d'un gestionnaire pour l'événement click
 
-boutonElt.addEventListener("click", function () {
-    console.log("clic");
-    top.location.href='Menu.html';
 
-});
-
-boutonElt2.addEventListener("click", function () {
-    console.log("clic2");
-    top.location.href='Calendrier.html';
-
-});
-
-boutonElt3.addEventListener("click", function () {
-    console.log("clic3");
-    top.location.href='Liste_Centres.html';
-});
-
-boutonElt4.addEventListener("click", function () {
-    console.log("clic4");
-    top.location.href='Liste_Patients.html';
-});
-boutonElt5.addEventListener("click", function () {
-    console.log("clic5");
-    top.location.href='Dossier_Patient.html';
-});
 boutonElt6.addEventListener("click", function () {
     console.log("clic6");
     top.location.href='Prise_RDV.html';
