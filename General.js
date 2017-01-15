af22dbe4d282d70298f47941fe044dd932142d97
Zizi30 <zineb.sen30@gmail.com>
2017-01-15 15:43:09 +0100

    Modifications Générales

--- General.js
+++ General.js
@@ -1,45 +1,30 @@
+
 var boutonElt1 = document.getElementById("menu1");
-var boutonElt2 = document.getElementById("menu5");
-var boutonElt3 = document.getElementById("menu3");
-var boutonElt4= document.getElementById("menu2");
-var boutonElt5 = document.getElementById("menu4");
-var boutonElt6 = document.getElementById("RetourDossierPatient");
+var boutonElt2 = document.getElementById("menu2");
+var boutonElt3= document.getElementById("menu3");
+var boutonElt4 = document.getElementById("menu4");
+var boutonElt5 = document.getElementById("RetourDossierPatient");
+
 
 boutonElt1.addEventListener("click", function () {
     console.log("clic1");
     console.log("Le récapitulatif a été envoyé");
-     top.location.href='Menu.html';
-    window.close();
+     top.location.href='Calendrier.html'; 
 });
-
 
 boutonElt2.addEventListener("click", function () {
     console.log("clic2");
-    console.log("Le récapitulatif a été envoyé");
-     top.location.href='Calendrier.html'; 
+     top.location.href='Liste_Services.html'; 
 });
 
 boutonElt3.addEventListener("click", function () {
     console.log("clic3");
     console.log("Le récapitulatif a été envoyé");
-    top.location.href='Liste_Centres.html'; 
+    top.location.href='Liste_Patients.html'; 
 });
 
 boutonElt4.addEventListener("click", function () {
     console.log("clic4");
     console.log("Le récapitulatif a été envoyé");
-    top.location.href='Liste_Patients.html';
+    top.location.href='Liste_Medecins.html';
 });
-
-boutonElt5.addEventListener("click", function () {
-    console.log("clic5");
-    console.log("Le récapitulatif a été envoyé");
-    top.location.href='Dossier_Patient.html';
-});
-
-boutonElt6.addEventListener("click", function () {
-    console.log("clic6");
-    console.log("Le récapitulatif a été envoyé");
-    top.location.href='Dossier_Patient.html';; 
-});
-
