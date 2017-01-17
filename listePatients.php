c8274b19f3ef104be8f909784d0c674324cb835f
melcash <melanie.billet@insa-lyon.fr>
2017-01-17 17:07:19 +0100

    Ajout autocomp liste patients

---
+++ listePatients.php
@@ -0,0 +1,30 @@
+<?php
+
+  try
+        {
+	       $bdd = new PDO('mysql:host=localhost;dbname=bdd_plateforme;charset=utf8', 'root', '');
+        }
+    catch(Exception $e)
+        {
+    die('Erreur : '.$e->getMessage());
+        }
+
+$term = $_GET['term'];
+
+$requete = $bdd->prepare('SELECT * FROM patient WHERE nom_p LIKE :term'); // j'effectue ma requête SQL grâce au mot-clé LIKE
+$requete->execute(array('term' => $term.'%'));
+
+$array = array(); // on créé le tableau
+
+while($donnee = $requete->fetch(PDO::FETCH_ASSOC)) // on effectue une boucle pour obtenir les données
+{
+  //  if (stripos($donnee[''], $_GET['term']) === 0) {
+        array_push($array, $donnee['nom_p']); // et on ajoute celles-ci à notre tableau
+
+   // }
+}
+
+
+echo json_encode($array); // il n'y a plus qu'à convertir en JSON
+
+?>
