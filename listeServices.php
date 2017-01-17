1c0d008845861cc346fc3f6ab888c1a6926c61a0
melcash <melanie.billet@insa-lyon.fr>
2017-01-17 17:24:36 +0100

    Autocomp recherche services
    attribut à chercher à redéfinir

---
+++ listeServices.php
@@ -0,0 +1,28 @@
+<?php
+
+  try
+        {
+	       $bdd = new PDO('mysql:host=localhost;dbname=bdd_plateforme;charset=utf8', 'root', '');
+        }
+    catch(Exception $e)
+        {
+    die('Erreur : '.$e->getMessage());
+        }
+
+$term = $_GET['term'];
+
+$requete = $bdd->prepare('SELECT * FROM service WHERE numSiret LIKE :term'); // j'effectue ma requête SQL grâce au mot-clé LIKE
+$requete->execute(array('term' => $term.'%'));
+
+$array = array(); // on créé le tableau
+
+while($donnee = $requete->fetch(PDO::FETCH_ASSOC)) // on effectue une boucle pour obtenir les données
+{
+    array_push($array, $donnee['numSiret']); // et on ajoute celles-ci à notre tableau
+
+}
+
+
+echo json_encode($array); // il n'y a plus qu'à convertir en JSON
+
+?>
