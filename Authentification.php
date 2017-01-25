9b616a3f1e620ab79e0dd45c17ce4e136e857468
Cyril <cyril.machillot@insa-lyon.fr>
2017-01-25 11:05:41 +0100

    modifs mineures authentification et nouveau service

--- Authentification.php
+++ Authentification.php
@@ -2,36 +2,31 @@
 <html >
 <head>
   <meta charset="UTF-8">
-  <title></title>
-  
-  
-  
-      <link rel="stylesheet" href="css/Authentification.css">
-
-  
+  <title>Authentification</title>
+  <link rel="stylesheet" href="css/Authentification.css">
 </head>
 
 <body>
-  <div class="login-page">
-  <div class="form">
-    <form class="register-form">
-      <input type="text" placeholder="Identifiant"/>
-      <input type="password" placeholder="Mot de passe"/>
-      <input type="text" placeholder="Email"/>
-      <button>create</button>
-      <p class="message">Déjà inscrit? <a href="#">Connexion</a></p>
-    </form>
-    <form class="login-form">
-      <input type="text" placeholder="Identifiant"/>
-      <input type="password" placeholder="Mot de passe"/>
-        <button><a href="Liste_Services.html"/>Se connecter</button>
-       <p class="message">Pas encore inscrit? <a href="#">Inscription</a></p>
-    </form>
-  </div>
-</div>
-  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-
+    <div class="login-page">
+        <div class="form">
+            <form class="register-form">
+                <input type="text" placeholder="Identifiant"/>
+                <input type="password" placeholder="Mot de passe"/>
+                <input type="text" placeholder="Email"/>
+                <button>create</button>
+                <p class="message">Déjà inscrit? <a href="#">Connexion</a></p>
+            </form>
+            <form class="login-form">
+                <input type="text" placeholder="Identifiant"/>
+                <input type="password" placeholder="Mot de passe"/>
+                <button><a href="Liste_Services.php">Se connecter</a></button>
+                <p class="message">Pas encore inscrit? <a href="#">Inscription</a></p>
+            </form>
+      </div>
+    </div>
+    
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
     <script src="js/Authentification.js"></script>
-
 </body>
+    
 </html>
