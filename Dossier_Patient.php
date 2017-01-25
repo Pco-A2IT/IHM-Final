87b4acc4b3797e74adf1d5bb6b015b69cc0b0d30
Zizi30 <zineb.sen30@gmail.com>
2017-01-25 12:09:23 +0100

    Modifications barre menu

--- Dossier_Patient.php
+++ Dossier_Patient.php
@@ -13,28 +13,32 @@
     <form action="AjoutBDD_dossierPatient.php" method="post">    
     <div class="gris">
               <div  class="gris2">
-             <div id="menu0" class="carreGris" ;>
-                <h4>Logout</h4>
-                <img class="icone_menu" src="Icones/logout.png"/>
+          <div id="menu0" class="carreGris" style="background-color:#1270B3";>
+                <h4>Patients</h4>    
+                <img class="icone_menu" src="Icones/patient_blanc.png"/>
             </div> 
             <div id="menu1" class="carreGris";>
                 <h4>Suivi</h4>
                 <img class="icone_suivi" src="Icones/recapitulatif.png"/>
             </div>
-            
-            <div id="menu2" class="carreGris";>
-                <h4>Services</h4>
-                <img class="icone_menu" src="Icones/hopital_blanc.png"/>
-            </div>
-                
-            <div id="menu3" class="carreGris" style="background-color:#1270B3";>
-                <h4>Patients</h4>    
-                <img class="icone_menu" src="Icones/patient_blanc.png"/>
-            </div> 
-            <div id="menu4" class="carreGris";>
+            <div id="menu2" class="carreGris" ;>
                 <h4>Médecins</h4>    
                 <img class="icone_menu" src="Icones/medecin_blanc.png"/>
             </div>
+                        
+            <div id="menu3" class="carreGris";>
+                <h4>Services</h4>
+                <img class="icone_menu" src="Icones/hopital_blanc.png"/>
+            </div>
+             <div id="menu4" class="carreGris">
+                <h4>Paramètres</h4>
+                <img class="icone_menu" src="Icones/parametres_blanc.png"/>      
+            </div>
+            <div id="menu5" class="carreGris">
+                <h4>Logout</h4>
+                <img class="icone_menu" src="Icones/logout.png"/>      
+            </div>
+                  
             
             <script src="js/General.js"></script>
         <div class="titre";   style="border-radius: 5px;">
--- Dossier_Service_modif.php
+++ Dossier_Service_modif.php
@@ -38,29 +38,32 @@ $req->closeCursor();
     <form action="ModifBDD_Service.php?idservice=<?php echo $_GET['idservice']; ?>" method="post">    
     <div class="gris">
               <div  class="gris2">
-             <div id="menu0" class="carreGris" ;>
-                <h4>Logout</h4>
-                <img class="icone_menu" src="Icones/logout.png"/>
+             <div id="menu0" class="carreGris";>
+                <h4>Patients</h4>    
+                <img class="icone_menu" src="Icones/patient_blanc.png"/>
             </div> 
             <div id="menu1" class="carreGris";>
                 <h4>Suivi</h4>
                 <img class="icone_suivi" src="Icones/recapitulatif.png"/>
             </div>
-            
-            <div id="menu2" class="carreGris" style="background-color:#1270B3";>
-                <h4>Services</h4>
-                <img class="icone_menu" src="Icones/hopital_blanc.png"/>
-            </div>
-                
-            <div id="menu3" class="carreGris";>
-                <h4>Patients</h4>    
-                <img class="icone_menu" src="Icones/patient_blanc.png"/>
-            </div> 
-            <div id="menu4" class="carreGris";>
+            <div id="menu2" class="carreGris" ;>
                 <h4>Médecins</h4>    
                 <img class="icone_menu" src="Icones/medecin_blanc.png"/>
             </div>
-            
+                        
+            <div id="menu3" class="carreGris" style="background-color:#1270B3";>
+                <h4>Services</h4>
+                <img class="icone_menu" src="Icones/hopital_blanc.png"/>
+            </div>
+             <div id="menu4" class="carreGris">
+                <h4>Paramètres</h4>
+                <img class="icone_menu" src="Icones/parametres_blanc.png"/>      
+            </div>
+            <div id="menu5" class="carreGris">
+                <h4>Logout</h4>
+                <img class="icone_menu" src="Icones/logout.png"/>      
+            </div>
+                  
             <script src="js/General.js"></script>
         <div class="titre";   style="border-radius: 5px;">
             <h1 class="titreGauche">Service</h1>
