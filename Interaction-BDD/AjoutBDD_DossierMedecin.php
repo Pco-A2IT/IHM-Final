bbf0d89bc556b0500660a09d844c3e04ef05faed
luigi-bardi <luigi.bardi1@gmail.com>
2017-01-25 22:43:13 +0100

    cc

--- Interaction-BDD/AjoutBDD_DossierMedecin.php
+++ Interaction-BDD/AjoutBDD_DossierMedecin.php
@@ -2,17 +2,33 @@
 // Connexion à la base de données
 include('../config.php');
 
+    $service_m=$_POST['service_m'];
+    echo "Le service rentré est ".$service_m;
+    
+    $centre_m=$_POST['centre_m'];
+    echo "Le service rentré est ".$centre_m;
 
+$req2 = $bdd->prepare('SELECT * FROM service WHERE centre_s = ? AND nom_s=? ');
+$req2->execute(array($centre_m, $service_m));
 
+if($centre_m!="" && $service_m!=""){
+while ($donn = $req2->fetch()){
+        $id_service=$donn['id_service'];
+        
+    }
+}
+else{
+    $id_service=0;
+}
+echo "essai".$id_service;
 
-
-$req = $bdd->prepare('INSERT INTO medecin(id_medecin, num_adeli_m ,id_service, civilite_m, nom_m, prenom_m, mail_m, ville_m, codePostal_m, adresse_m, telephone_m) VALUES(NULL, ? ,111, ?,?,?,?,?,?,?,?)');
+$req = $bdd->prepare('INSERT INTO medecin(id_medecin, num_adeli_m ,id_service, civilite_m, nom_m, prenom_m, mail_m, ville_m, codePostal_m, adresse_m, telephone_m) VALUES(NULL, ? ,?, ?,?,?,?,?,?,?,?)');
 
 
 
 //$req = $bdd->exec('INSERT INTO medecin(id_medecin,num_adeli_m,id_service, civilite_m, nom_m, prenom_m, mail_m, ville_m, codePostal_m, adresse_m, telephone_m) VALUES(NULL, ?,1111, ?,?,?,?,?,?,? ,?)');
 
-$req->execute(array($_POST['num_adeli_m'], $_POST['civilite_m'], $_POST['nom_m'], $_POST['prenom_m'], $_POST['email_m'],  $_POST['ville_m'],  $_POST['codePostal_m'],$_POST['adresse_m'] ,$_POST['telephone_m']));
+$req->execute(array($_POST['num_adeli_m'], $id_service, $_POST['civilite_m'], $_POST['nom_m'], $_POST['prenom_m'], $_POST['email_m'],  $_POST['ville_m'],  $_POST['codePostal_m'],$_POST['adresse_m'] ,$_POST['telephone_m']));
 
 
 
