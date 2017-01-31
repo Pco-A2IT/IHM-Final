836ca36bfa8a1f59e59e7ef6240062c312977baf
MengSU6 <meng.su@insa-lyon.fr>
2017-01-31 21:09:12 +0100

    table

--- Recapitulatif.php
+++ Recapitulatif.php
@@ -45,7 +45,7 @@
                       <script language="JavaScript">writeSource("js10");</script>
                     </p>
                     <div class="section1">
-                            &nbsp;&nbsp;&nbsp;<div id="titles"> 
+                            <div id="titles"> 
                                 <span class="title active" target="ongletC1">Suivi RDV</span> 
                                 <span class="title" target="ongletC2">Envoi CR </span>
                             </div>
