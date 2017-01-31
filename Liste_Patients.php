b3491f79a4fc54fe7f41d9fe1a6a2d3743cc0ec7
MengSU6 <meng.su@insa-lyon.fr>
2017-01-31 20:28:08 +0100

    disposition du table

--- Liste_Patients.php
+++ Liste_Patients.php
@@ -72,8 +72,7 @@
                             <a href="Dossier_Patient.php" class="myButton1"><img class="icone_ajouter" src="Icones/button_ajouter.png"> Ajouter Patient</a>
                 </div>
                 <div class="onglet"> 
-                    <div class="position_table">
-                    <table align="center" cellspacing='5px' id="tbl" class="table">   
+                    <table cellspacing='5px' id="tbl" class="table">   
                         <th>Fiche</th>
                         <th>Nom </th>
                         <th>Prénom </th>
@@ -148,7 +147,7 @@ while($dnn = $pdo_select->fetch() )
                             <td><img class="supprimer" src="button_supprimer.png"></td>
                         </tr> -->
                     </table> 
-                    </div>
+                    
                 </div>      
             </div>
         </div>
