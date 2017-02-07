134f152b04df7915fbdbecbe97b9396222811adc
Zizi30 <zineb.sen30@gmail.com>
2017-02-07 14:45:07 +0100

    Ajout Table Examens dans Prise RDV

--- Prise_RDV.php
+++ Prise_RDV.php
@@ -1,6 +1,8 @@
+<?php
+   include('config.php');
+?>
 <html>
 <head>
-    <meta charset="UTF-8">
    <link href="css/General.css"type="text/css"rel="stylesheet"/>    <!-- BOOTSTRAP -->
 </head>
 <body>
@@ -51,36 +53,97 @@
               <br><br>
               
              <h4 style='color:grey padding-left:2; margin-top:10; margin-bottom:10'>Examens</h4>
-                          <input type="checkbox" id="checkbox-1-1" class="regular-checkbox" /><label for="checkbox-1-1"></label>&nbsp;IRM&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                          <input type="checkbox" id="checkbox-1-2" class="regular-checkbox" /><label for="checkbox-1-2"></label>&nbsp;Bilan Cardiaque&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                          <input type="checkbox" id="checkbox-1-3" class="regular-checkbox" /><label for="checkbox-1-3"></label>&nbsp;RDV neurologue&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+             <form action="Prise_RDV.php" method="post">
+                          <input type="checkbox" name="choix1" class="regular checkbox" value="on"/><label for="checkbox-1"></label>&nbsp;IRM&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                          <input type="checkbox" name="choix2" class="regular checkbox" value="on"/><label for="checkbox-1"></label>&nbsp;Bilan Cardiaque&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                          <input type="checkbox" name="choix3" class="regular checkbox" value="on"/><label for="checkbox-1"></label>&nbsp;RDV neurologue&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                          <td align="center"  colspan="2">
+                                <input align="center" type="submit" accesskey="enter" value="Rechercher" id="btn" onmousemove="changeBgColor('btn')" onmouseout="recoverBgColor('btn');" class="submit" formmethod="post"/> 
+                          </td>
+             </form>
+             
+              <?php
                 
+                
+                if($_POST["choix1"]=="on"){
+                $irm='YES';
+              }else{
+                $irm='NO';
+              }
+              if($_POST["choix2"]=="on"){
+                $cardiaque="YES";
+              }else{
+                $cardiaque="NO";
+              }
+              if($_POST["choix3"]=="on"){
+                $neuro="YES";
+              }else{
+                $neuro="NO";
+              }
+              
+              
+              if($irm=="YES"){
+                //$reponse = $bdd->query('SELECT * FROM Service WHERE irm_s LIKE :YES');
+                
+                
+              $reponse=$bdd->query('SELECT * FROM service WHERE irm_s=\'YES\' ');
+             
+               
+                    
+              }
+              if($cardiaque=="YES"){
+                $reponse = $bdd->query('SELECT * FROM service WHERE cardiaque_s=\'YES\'');
+                
+              }
+              if($neuro=="YES"){
+                $reponse = $bdd->query('SELECT * FROM service WHERE neuro_s=\'YES\'');
+               
+              }
+              ?>
             <br>
             <br>
               <div class="div3">
                   <h4 style='color:grey padding-left:2; margin-top:10; margin-bottom:10'>Résultats Recherche</h4>
                   <table align="right" cellspacing="5px" class="table"> 
                       <tr><th>Centres</th><th>Adresse</th><th>Contact</th><th>Examens</th><th>Jour</th><th>Horaire</th><th>Planifié</th></tr>
-                      <tr><td rowspan="3">Hospitalisation de jour</td><td rowspan="3">20 avenue Albert</td><td rowspan="3">0478654312</td><td> IRM </td>
+                      <?php
+                    while ($donnees = $reponse->fetch()){
+                        ?>
+                      <tr><td rowspan="3"><?php echo $donnees[centre_s]; ?></td><td rowspan="3"><?php echo $donnees['adresse_s']; ?></td><td rowspan="3"><?php echo $donnees['telephone_s']; ?></td><td> IRM </td>
                        <td>
                            <label for="date"></label><input id="date" type="date" value=""/></td>
                            <td><label for="heure"></label><input id="heure" type="time" value=""/></td>       
-                           <td><input type="checkbox" id="checkbox-2-1" class="regular-checkbox" /><label for="checkbox-2-1"></label></td>
+                           <td><input type="checkbox" id="checkbox-1" class="regular-checkbox" /><label for="checkbox-1"></label></td>
                       </tr>
                       <tr>
+                    <?php
+                       // if($donnees['cardiaque_s']=="YES"){
+                    ?>
                       <td> Bilan Cardiaque </td>
                        <td>
                            <label for="date"></label><input id="date" type="date" value=""/></td>
                            <td><label for="heure"></label><input id="heure" type="time" value=""/></td>       
-                           <td><input type="checkbox" id="checkbox-2-2" class="regular-checkbox" /><label for="checkbox-2-2"></label></td>
+                           <td><input type="checkbox" id="checkbox-2" class="regular-checkbox" /><label for="checkbox-2"></label></td>
                       </tr>
                        <tr>
+                    <?php
+                        //}
+                    ?>
+                    <?php
+                        //if($donnees['neuro_s']=="YES"){
+                    ?>
                       <td> RDV neurologue </td>
                        <td>
                            <label for="date"></label><input id="date" type="date" value=""/></td>
                            <td><label for="heure"></label><input id="heure" type="time" value=""/></td>       
-                           <td><input type="checkbox" id="checkbox-2-3" class="regular-checkbox" /><label for="checkbox-2-3"></label></td>
+                           <td><input type="checkbox" id="checkbox-3" class="regular-checkbox" /><label for="checkbox-3"></label></td>
                       </tr>
+                    <?php
+                      //}
+                      ?>
+                    <?php
+                        }
+                    ?>
                       </table>
               </div>
               </div>
