9822551236c40c7b74d14c1651933aa2353287f0
MengSU6 <meng.su@insa-lyon.fr>
2017-02-09 21:25:00 +0100

    disposition

--- Liste_Examens.php
+++ Liste_Examens.php
@@ -50,7 +50,7 @@
     <div class="blanc";   style="border-radius: 5px;">
         <table cellspacing="0px" id="tbl" class="table" style="margin-top:70px;">  
                         
-                        <th align="right">Nom Examen </th>
+                        <th style="align:right;">Nom Examen </th>
                         <th>Description </th>
         <?php
             $reponse = $bdd->query('SELECT * FROM Examen');
