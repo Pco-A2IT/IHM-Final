ac0bf38bd77677a64121e8bf97e83101fed6c18e
MengSU6 <meng.su@insa-lyon.fr>
2017-03-03 10:02:44 +0100

    export

--- exportCSV.php
+++ exportCSV.php
@@ -1,19 +1,40 @@
+<?php
+include 'PHPExcel.php';
+include 'PHPExcel/Writer/Excel2007.php';
+
+$objPHPExcel = new PHPExcel();
+$objWriter = new PHPExcel_Writer_Excel2007($objPHPExcel);
+$objWriter->save("patient.xlsx");
+
+$conn = mysql_connect("localhost","root","") or die("fail to connect！");   
+mysql_select_db("image",$conn); 
+$sql = mysql_query("select * from test");
+
+$objPHPExcel->setActiveSheetIndex(0)
+            ->setCellValue('A1', 'Fiche')
+            ->setCellValue('B1', 'Nom')
+            ->setCellValue('C1', 'Prénom')
+            ->setCellValue('D1', 'Date de naissance')
+            ->setCellValue('E1', 'Code postal');
+            ->setCellValue('F1', 'Ville')
+            ->setCellValue('G1', 'Téléphone');
+
 $db = new Mysql($dbconfig);
-$sql = "SELECT * FROM  patient";
-$row = $db->GetAll($sql);
+$sql = "SELECT * FROM patient";
+$row = $db->GetAll($sql); 
 $count = count($row);
 for ($i = 2; $i <= $count+1; $i++) {
- $objPHPExcel->getActiveSheet()->setCellValue('A' . $i, convertUTF8($row[$i-2][1]));
- $objPHPExcel->getActiveSheet()->setCellValue('B' . $i, convertUTF8($row[$i-2][2]));
- $objPHPExcel->getActiveSheet()->setCellValue('C' . $i, convertUTF8($row[$i-2][3]));
- $objPHPExcel->getActiveSheet()->setCellValue('D' . $i, convertUTF8($row[$i-2][4]));
- $objPHPExcel->getActiveSheet()->setCellValue('E' . $i, convertUTF8(date("Y-m-d", $row[$i-2][5])));
- $objPHPExcel->getActiveSheet()->setCellValue('F' . $i, convertUTF8($row[$i-2][6]));
- $objPHPExcel->getActiveSheet()->setCellValue('G' . $i, convertUTF8($row[$i-2][7]));
- $objPHPExcel->getActiveSheet()->setCellValue('H' . $i, convertUTF8($row[$i-2][8]));
+ $objPHPExcel->getActiveSheet()->setCellValue('A1' . $i, convertUTF8($row[$i-2][1]));
+ $objPHPExcel->getActiveSheet()->setCellValue('B1' . $i, convertUTF8($row[$i-2][2]));
+ $objPHPExcel->getActiveSheet()->setCellValue('C1' . $i, convertUTF8($row[$i-2][3]));
+ $objPHPExcel->getActiveSheet()->setCellValue('D1' . $i, convertUTF8(date("d-m-Y", $row[$i-2][5])))
+ $objPHPExcel->getActiveSheet()->setCellValue('E1' . $i, convertUTF8($row[$i-2][5]));
+ $objPHPExcel->getActiveSheet()->setCellValue('F1' . $i, convertUTF8($row[$i-2][6]));
+ $objPHPExcel->getActiveSheet()->setCellValue('G1' . $i, convertUTF8($row[$i-2][7]));
 }
  
 echo date('H:i:s') . " Create new Worksheet object\n";
-$objPHPExcel->createSheet();
-$objWriter = PHPExcel_IOFactory::createWriter($objExcel, 'Excel5');
-$objWriter-save('php://output');
+$objPHPExcel->getActiveSheet()->setTitle('patient');      //设置sheet的名称
+$objPHPExcel->setActiveSheetIndex(0);                            //设置sheet的起始位置
+$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');   //通过PHPExcel_IOFactory的写函数将上面数据写出来
+?>
