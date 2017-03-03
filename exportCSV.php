4de379fb08b36b3fbefd8eec5326ace5038f3d19
MengSU6 <meng.su@insa-lyon.fr>
2017-03-03 10:19:34 +0100

    export

--- exportCSV.php
+++ exportCSV.php
@@ -27,7 +27,7 @@ for ($i = 2; $i <= $count+1; $i++) {
  $objPHPExcel->getActiveSheet()->setCellValue('A1' . $i, convertUTF8($row[$i-2][1]));
  $objPHPExcel->getActiveSheet()->setCellValue('B1' . $i, convertUTF8($row[$i-2][2]));
  $objPHPExcel->getActiveSheet()->setCellValue('C1' . $i, convertUTF8($row[$i-2][3]));
- $objPHPExcel->getActiveSheet()->setCellValue('D1' . $i, convertUTF8(date("d-m-Y", $row[$i-2][4])))
+ $objPHPExcel->getActiveSheet()->setCellValue('D1' . $i, convertUTF8(date("d-m-Y", $row[$i-2][4])));
  $objPHPExcel->getActiveSheet()->setCellValue('E1' . $i, convertUTF8($row[$i-2][5]));
  $objPHPExcel->getActiveSheet()->setCellValue('F1' . $i, convertUTF8($row[$i-2][6]));
  $objPHPExcel->getActiveSheet()->setCellValue('G1' . $i, convertUTF8($row[$i-2][7]));
