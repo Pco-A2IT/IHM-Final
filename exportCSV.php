8feb501638a38f9c44dd498aeff5b98f3e1afbc3
MengSU6 <meng.su@insa-lyon.fr>
2017-03-03 10:52:17 +0100

    export

--- exportCSV.php
+++ exportCSV.php
@@ -18,16 +18,17 @@ $objPHPExcel->setActiveSheetIndex(0)
             ->setCellValue('F1', 'Ville')
             ->setCellValue('G1', 'Téléphone');
 
-$row = $db->GetAll($sql); 
-$count = count($row);
-for ($i = 2; $i <= $count+1; $i++) {
- $objPHPExcel->getActiveSheet()->setCellValue('A1' . $i, convertUTF8($row[$i-2][1]));
- $objPHPExcel->getActiveSheet()->setCellValue('B1' . $i, convertUTF8($row[$i-2][2]));
- $objPHPExcel->getActiveSheet()->setCellValue('C1' . $i, convertUTF8($row[$i-2][3]));
- $objPHPExcel->getActiveSheet()->setCellValue('D1' . $i, convertUTF8(date("d-m-Y", $row[$i-2][4])));
- $objPHPExcel->getActiveSheet()->setCellValue('E1' . $i, convertUTF8($row[$i-2][5]));
- $objPHPExcel->getActiveSheet()->setCellValue('F1' . $i, convertUTF8($row[$i-2][6]));
- $objPHPExcel->getActiveSheet()->setCellValue('G1' . $i, convertUTF8($row[$i-2][7]));
+//$row = $bdd->GetAll($sql); 
+//$count = count($row);
+$i=2;                //定义一个i变量，目的是在循环输出数据是控制行数
+while($rs=mysql_fetch_array($sql)){
+ $objPHPExcel->getActiveSheet()->setCellValue('A1' . $i,  $rs[0]);
+ $objPHPExcel->getActiveSheet()->setCellValue('B1' . $i,  $rs[1]);
+ $objPHPExcel->getActiveSheet()->setCellValue('C1' . $i,  $rs[2]);
+ $objPHPExcel->getActiveSheet()->setCellValue('D1' . $i,  date("d-m-Y", $rs[3]));
+ $objPHPExcel->getActiveSheet()->setCellValue('E1' . $i,  $rs[4]);
+ $objPHPExcel->getActiveSheet()->setCellValue('F1' . $i,  $rs[5]);
+ $objPHPExcel->getActiveSheet()->setCellValue('G1' . $i,  $rs[6]);
 }
  
 echo date('H:i:s') . " Create new Worksheet object\n";
