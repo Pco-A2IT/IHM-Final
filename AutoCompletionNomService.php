514833dbf4535c6d5fdc64e67d48db23a0c3162f
melcash <melanie.billet@insa-lyon.fr>
2017-03-20 18:31:27 +0100

    autocompletion service du médecin

---
+++ AutoCompletionNomService.php
@@ -0,0 +1,10 @@
+<?php
+// Les propriétés doivent être public pour qu'elle puisse entre lu par la méthode de transformation en JSON.
+class AutoCompletionNomService {
+	public $nom_s;
+	public $centre_s;
+	public $adresse_s;
+	public $codePostal_s;
+    public $ville_s;
+}
+?>
