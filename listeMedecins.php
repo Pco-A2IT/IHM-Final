3ddf449b4f904f5cdb7c10d5e21a262887df34c7
melcash <melanie.billet@insa-lyon.fr>
2017-03-24 09:48:28 +0100

    listes autocomplétion commentées

--- listeMedecins.php
+++ listeMedecins.php
@@ -2,22 +2,13 @@
 
 <?php
     include('config.php'); //connexion à la bdd
+    $term = $_GET['term'];
+    $requete = $bdd->prepare('SELECT * FROM medecin WHERE nom_m LIKE :term'); 
+    $requete->execute(array('term' => $term.'%')); // requête SQL : les noms de médecins commençant par le terme saisi
+    $array = array(); // création d'un tableau
 
-
-$term = $_GET['term'];
-
-$requete = $bdd->prepare('SELECT * FROM medecin WHERE nom_m LIKE :term'); 
-$requete->execute(array('term' => $term.'%')); // requête SQL : les noms de médecins commençant par le terme saisi
-
-$array = array(); // ocréation d'un tableau
-
-while($donnee = $requete->fetch(PDO::FETCH_ASSOC)) // on effectue une boucle pour obtenir les données
-{
-    array_push($array, $donnee['nom_m']); // on ajoute les données à notre tableau
-
-}
-
-
-echo json_encode($array); // on convertit le tableau en format json
-
+    while($donnee = $requete->fetch(PDO::FETCH_ASSOC)) { // on effectue une boucle pour obtenir les données
+        array_push($array, $donnee['nom_m']); // on ajoute les données à notre tableau
+    }
+    echo json_encode($array); // on convertit le tableau en format json
 ?>
