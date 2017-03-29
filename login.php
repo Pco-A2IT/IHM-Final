6c6c728b23c8a74d71d9170cc0695c6b8895c20a
Zizi30 <zineb.sen30@gmail.com>
2017-03-29 17:00:36 +0200

    Authentification

--- login.php
+++ login.php
@@ -39,7 +39,7 @@ if(!empty($_POST) && !empty($_POST['username']) && !empty($_POST['password'])){
                     <input type="text" name="username" class="form-control" placeholder="Identifiant"/>
                     <input type="password" name="password" class="form-control" placeholder="Mot de passe"/>
                     <input type="submit" accesskey="enter" value="Se connecter" onmousemove="changeBgColor('btn')" onmouseout="recoverBgColor('btn');"  formmethod="post" style="text-align:center"/> 
-                    <li><a href="register.php">Pas encore inscrit ?</a></li>
+                    <a href="register.php" style="text-decoration:none">Pas encore inscrit ?</a>
                 </div>
             </div>
         </form>
